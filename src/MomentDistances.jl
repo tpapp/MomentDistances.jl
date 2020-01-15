eb54ff0d651965cf9202b6b1a9e0b4e0e8a41fea
Tamas K. Papp <tkpapp@gmail.com>
2020-01-15 19:03:22 +0100

    basic summaries

--- src/MomentDistances.jl
+++ src/MomentDistances.jl
@@ -1,9 +1,16 @@
 module MomentDistances
 
+export distance, NamedSum, Weighted, ElementwiseMean, AbsoluteRelative, summarize
+
 using ArgCheck: @argcheck
 using LinearAlgebra: LinearAlgebra
+using Base.Multimedia: MIME, @MIME_str
 using Statistics: mean
 using UnPack: @unpack
+
+####
+#### core functionality
+####
 
 function distance end
 
@@ -54,5 +61,94 @@ function distance(metric::AbsoluteRelative, x, y)
         Δ / (relative_adjustment == Inf ? A : max(1, relative_adjustment * A))
     end
 end
+
+####
+#### summaries
+####
+#### FIXME the whole code below is probably a weird abuse of the multimedia I/O system,
+#### revamp when summarizing for other outputs. `summary` should be fully internal, an
+#### implementation detail constructing a summary, `show` methods should be defined for a
+#### wrapper type?
+
+"""
+    summary([options], [mime], metric, x, y)
+
+Return summary of how a metric was calculated, according to the specific MIME type.
+"""
+summary(metric, x, y) = summary(MIME("text/plain"), metric, x, y)
+
+###
+### plain text summaries
+###
+
+function summary(mime::MIME"text/plain", metric, x, y)
+    summary(TextSummaryOptions(), mime, metric, x, y)
+end
+
+"Options for printing text summaries."
+Base.@kwdef struct TextSummaryOptions
+    sigdigits::Int = 2
+end
+
+function _dotted_repr(options, x)
+    lines = split(repr(x), '\n')
+    line1 = first(lines)
+    length(lines) > 1 ? line1 * "…" : line1
+end
+
+function _dotted_repr(options::TextSummaryOptions, x::Real)
+    repr(round(x; sigdigits = options.sigdigits))
+end
+
+function _indent(str, n = 2)
+    join(map(line -> ' '^n * line, split(str, '\n')), '\n')
+end
+
+"""
+A hack to avoid printing quotes for strings while utilizing Julia's array printing
+machinery.
+"""
+struct _NoQuotes{T}
+    str::T
+end
+
+Base.show(io::IO, nq::_NoQuotes) = print(io, nq.str)
+
+function summary(options, ::MIME"text/plain", metric, x, y)
+    # this is the fallback, and also the indended summary for scalar metrics like
+    # AbsoluteRelative
+    x_, y_, d_ = _dotted_repr.(Ref(options), (x, y, distance(metric, x, y)))
+    "‹" * x_ * " → " * y_ * ": " * d_ * "›"
+end
+
+function summary(options, mime::MIME"text/plain", metric::ElementwiseMean, x, y)
+    "elementwise mean distance: " * _dotted_repr(options, distance(metric, x, y)) * "\n" *
+        _indent(sprint(Base.print_array,
+                       map((x, y) -> _NoQuotes(summary(options, mime, metric.metric, x, y)),
+                           x, y)))
+end
+
+function summary(options, mime::MIME"text/plain", metric::Weighted, x, y)
+    "weighted: " * _dotted_repr(options, distance(metric, x, y)) * '\n' *
+        _indent(summary(options, mime, metric.metric, x, y))
+end
+
+function summary(options, mime::MIME"text/plain", metric::NamedSum, x, y)
+    str = "total: " * _dotted_repr(options, distance(metric, x, y))
+    for (key, metric) in pairs(metric.named_metrics)
+        str *= "\n" * _indent("from $(key):\n" *
+                              _indent(summary(options, mime, metric,
+                                              getproperty(x, key), getproperty(y, key))))
+    end
+    str
+end
+
+function summarize(io, mime::MIME"text/plain", metric, x, y)
+    print(io, summary(mime, metric, x, y))
+end
+
+summarize(io, metric, x, y) = summarize(io, MIME("text/plain"), metric, x, y)
+
+summarize(metric, x, y) = summarize(stdout, metric, x, y)
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using MomentDistances: distance, AbsoluteRelative, ElementwiseMean, NamedSum, Weighted
+using MomentDistances
 using Statistics: mean
 using Test
 
@@ -34,4 +34,39 @@ end
 
 @testset "constructor checks" begin
     @test_throws ArgumentError Weighted(AbsoluteRelative(), -9)
+end
+
+@testset "text summaries" begin
+    s1 = 1
+    s2 = 2
+    ms = AbsoluteRelative()
+    A1 = ones(2, 2)
+    A2 = reshape(0:3, 2, 2)
+    mA = ElementwiseMean(AbsoluteRelative())
+
+    @test MomentDistances.summary(ms, s1, s2) == "‹1.0 → 2.0: 1.0›"
+    @test MomentDistances.summary(mA, A1, A2) ==
+        """
+        elementwise mean distance: 1.0
+           ‹1.0 → 0.0: 1.0›  ‹1.0 → 2.0: 1.0›
+           ‹1.0 → 1.0: 0.0›  ‹1.0 → 3.0: 2.0›"""
+    @test MomentDistances.summary(Weighted(mA, 0.7), A1, A2) ==
+        """
+        weighted: 0.7
+          elementwise mean distance: 1.0
+             ‹1.0 → 0.0: 1.0›  ‹1.0 → 2.0: 1.0›
+             ‹1.0 → 1.0: 0.0›  ‹1.0 → 3.0: 2.0›"""
+    @test MomentDistances.summary(NamedSum((s = ms, A = Weighted(mA, 0.7))),
+                                  (s = s1, A = A1), (s = s2, A = A2)) ==
+        """
+        total: 1.7
+          from s:
+            ‹1.0 → 2.0: 1.0›
+          from A:
+            weighted: 0.7
+              elementwise mean distance: 1.0
+                 ‹1.0 → 0.0: 1.0›  ‹1.0 → 2.0: 1.0›
+                 ‹1.0 → 1.0: 0.0›  ‹1.0 → 3.0: 2.0›"""
+
+    @test sprint(summarize, ms, s1, s2) == MomentDistances.summary(ms, s1, s2)
 end
